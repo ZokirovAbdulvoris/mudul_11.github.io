e6583998a756a64a31898c58fdeaf743d045e027
VICTUS <abduvoruszokirov@gmail.com>
2024-05-07 15:27:28 +0500

    start project

--- keybords.py
+++ keybords.py
@@ -1,6 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
-web_up = WebAppInfo(url="https://anvar0037.github.io/modul_11.github.io/")
+web_up = WebAppInfo(url="https://zokirovabdulvoris.github.io/mudul_11.github.io/")
 
 
 app_kb = ReplyKeyboardMarkup(keyboard=[
