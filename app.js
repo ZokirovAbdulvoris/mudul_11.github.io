b728c0d191187fbff09d0018b3550e23d053e521
VICTUS <abduvoruszokirov@gmail.com>
2024-05-09 16:43:17 +0500

    start project

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
-let btn5 = document.getElementById("btn5");
-let btn6 = document.getElementById("btn6");
 
 let item = " "
 
@@ -17,35 +15,23 @@ let item = " "
 
 
 btn1.addEventListener("click", function () {
-    tg.MainButton.setText("btn1 bosildi");
-    item = "button1 bosildi";
+    tg.MainButton.setText("Burger bosildi");
+    item = "Burger bosildi";
     tg.MainButton.show();
 });
 btn2.addEventListener("click", function () {
-    tg.MainButton.setText("btn2 bosildi");
-    item = "button2 bosildi";
+    tg.MainButton.setText("KFC bosildi");
+    item = "KFC bosildi";
     tg.MainButton.show();
 });
 btn3.addEventListener("click", function () {
-    tg.MainButton.setText("btn3 bosildi");
-    item = "button3 bosildi";
+    tg.MainButton.setText("Lavash bosildi");
+    item = "Lavash bosildi narxi 8$";
     tg.MainButton.show();
 });
 btn4.addEventListener("click", function () {
-    tg.MainButton.setText("btn4 bosildi");
-    item = "button4 bosildi";
-
-    tg.MainButton.show();
-});
-btn5.addEventListener("click", function () {
-    tg.MainButton.setText("btn5 bosildi");
-    item = "button5 bosildi";
-
-    tg.MainButton.show();
-});
-btn6.addEventListener("click", function () {
-    tg.MainButton.setText("btn6 bosildi");
-    item = "button6 bosildi";
+    tg.MainButton.setText("Fri bosildi");
+    item = "Fri bosildi";
 
     tg.MainButton.show();
 });
