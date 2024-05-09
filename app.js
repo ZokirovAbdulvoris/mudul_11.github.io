7bee937c4470edd077176120b59d08dd4158271d
VICTUS <abduvoruszokirov@gmail.com>
2024-05-09 16:27:26 +0500

    start project

--- app.js
+++ app.js
@@ -2,44 +2,54 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 
 tg.MainButton.textColor = "#FFFFFF"
-tg.MainButton.color = "#00FF08"
+tg.MainButton.color = "#FC3005"
 
-let btn1 = document.getElementById("btn1")
-let btn2 = document.getElementById("btn2")
-let btn3 = document.getElementById("btn3")
-let btn4 = document.getElementById("btn4")
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
-let item = ""
+let item = " "
 
-btn1.addEventListener("click", function(){
-    tg.MainButton.setText("btn1 bosildi")
+
+
+
+btn1.addEventListener("click", function () {
+    tg.MainButton.setText("btn1 bosildi");
     item = "button1 bosildi";
     tg.MainButton.show();
-
-})
-
-btn2.addEventListener("click", function(){
-    tg.MainButton.setText("btn2 bosildi")
+});
+btn2.addEventListener("click", function () {
+    tg.MainButton.setText("btn2 bosildi");
     item = "button2 bosildi";
     tg.MainButton.show();
-
-})
-
-btn3.addEventListener("click", function(){
-    tg.MainButton.setText("btn3 bosildi")
+});
+btn3.addEventListener("click", function () {
+    tg.MainButton.setText("btn3 bosildi");
     item = "button3 bosildi";
     tg.MainButton.show();
-
-})
-
-btn4.addEventListener("click", function(){
-    tg.MainButton.setText("btn4 bosildi")
+});
+btn4.addEventListener("click", function () {
+    tg.MainButton.setText("btn4 bosildi");
     item = "button4 bosildi";
+
     tg.MainButton.show();
+});
+btn5.addEventListener("click", function () {
+    tg.MainButton.setText("btn5 bosildi");
+    item = "button5 bosildi";
 
-})
+    tg.MainButton.show();
+});
+btn6.addEventListener("click", function () {
+    tg.MainButton.setText("btn6 bosildi");
+    item = "button6 bosildi";
 
-Telegram.WebApp.onEvent("mainButtonClicked", function (){
+    tg.MainButton.show();
+});
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
     tg.sendData(item)
-})
-
+});
--- handlers.py
+++ handlers.py
@@ -1,24 +1,22 @@
+
 from aiogram import Dispatcher, types, F
-from aiogram.filters import CommandStart
+from aiogram.enums import ContentType
+from aiogram.filters import CommandStart, MagicData, callback_data
+from aiogram.utils.magic_filter import MagicFilter
 
 from keybords import app_kb
 
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
-async def start(message: types.Message):
-    await message.answer("Salom", reply_markup=app_kb)
+async def start(msg: types.Message):
+    await msg.answer("Salom", reply_markup=app_kb)
 
 
-@dp.message(F.func(lambda message: message.web_app_data.data == "TestMessage"))
-async def get_btn(message: types.Message):
-    await message.answer(message.web_app_data.data)
-
-
-
-
-
-
+@dp.message(F.func(lambda msg: msg.web_app_data.data))
+async def get_btn(msg: types.Message):
+    await msg.answer(msg.web_app_data.data)
 
 
 
