b0cdc1854d3d21caa8b07b6f0b9ed80a633ff172
VICTUS <abduvoruszokirov@gmail.com>
2024-05-09 15:40:31 +0500

    start project

--- app.js
+++ app.js
@@ -9,31 +9,37 @@ let btn2 = document.getElementById("btn2")
 let btn3 = document.getElementById("btn3")
 let btn4 = document.getElementById("btn4")
 
+let item = ""
 
 btn1.addEventListener("click", function(){
     tg.MainButton.setText("btn1 bosildi")
-    tg.MainButton.show()
+    item = "button1 bosildi";
+    tg.MainButton.show();
 
 })
 
 btn2.addEventListener("click", function(){
     tg.MainButton.setText("btn2 bosildi")
-    tg.MainButton.show()
+    item = "button2 bosildi";
+    tg.MainButton.show();
 
 })
 
 btn3.addEventListener("click", function(){
     tg.MainButton.setText("btn3 bosildi")
-    tg.MainButton.show()
+    item = "button3 bosildi";
+    tg.MainButton.show();
 
 })
 
 btn4.addEventListener("click", function(){
     tg.MainButton.setText("btn4 bosildi")
-    tg.MainButton.show()
+    item = "button4 bosildi";
+    tg.MainButton.show();
 
 })
 
 Telegram.WebApp.onEvent("mainButtonClicked", function (){
-    tg.sendData("TestMessage")
+    tg.sendData(item)
 })
+
