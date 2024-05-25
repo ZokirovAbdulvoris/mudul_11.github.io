781529d50e16b327d9f5d488efbc8e7d50e0efa2
VICTUS <abduvoruszokirov@gmail.com>
2024-05-25 15:04:05 +0500

    start project

--- app.js
+++ app.js
@@ -204,7 +204,37 @@ abtn7.addEventListener("click", function () {
     tg.MainButton.show();
 });
 
+
+// -------------------8-------------------
+
+count8 = document.getElementById("count8");
+rbtn8 = document.getElementById("rbtn8");
+abtn8 = document.getElementById("abtn8");
+let item8 = "";
+let n_count8 = 0;
+rbtn7.addEventListener("click", function () {
+    if (n_count8 > 0) {
+        count7.innerText = n_count8 -= 1;
+    }
+
+    count8.style.display = "inline-block";
+    item8 = "Hot-Dog/14000/" + count8.innerText;
+
+    tg.MainButton.setText("Tolov");
+    tg.MainButton.show();
+});
+
+
+abtn8.addEventListener("click", function () {
+    count8.innerText = n_count8 += 1;
+    count8.style.display = "inline-block";
+    item8 = "Hot-Dog/14000/" + count8.innerText;
+
+    tg.MainButton.setText("Tolov");
+    tg.MainButton.show();
+});
+
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    data = item1 + "|" + item2 + "|" + item3 + "|" + item4 + "|" + item5 + "|" + item6 + "|" + item7 + "|"
+    data = item1 + "|" + item2 + "|" + item3 + "|" + item4 + "|" + item5 + "|" + item6 + "|" + item7 + "|" + item8 + "|"
     tg.sendData(data);
 });
